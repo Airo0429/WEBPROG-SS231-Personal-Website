d86e4880a9f66932394dec1c142bd119e80a7b55
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2024-12-17 08:36:22 +0800

    comirtttt

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 
 function myFunction2() {
-    const element = document.getElementsByTagName("h1");
+    const element = document.getElementsByTagName("p");
  
-document.getElementById("demo").innerHTML = 'The text in first h1 tag (index 0) is: ' + element[0].innerHTML;
+document.getElementById("demo").innerHTML = 'The text in first p tag (index 0) is: ' + element[0].innerHTML;
 }
