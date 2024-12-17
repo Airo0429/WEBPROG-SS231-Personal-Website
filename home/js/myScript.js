365a9252a951e37ff8d91b80f696d69b127a31fd
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2024-12-17 08:42:12 +0800

    updated h1 to br

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 
 function myFunction2() {
-    const element = document.getElementsByTagName("h1");
+    const element = document.getElementsByTagName("br");
  
     document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
  
