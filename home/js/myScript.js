3214d5d8341ff2ce8f3da2445a88c85e4023f52d
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2024-12-17 08:44:23 +0800

    going back to h1

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 
 function myFunction2() {
-    const element = document.getElementsByTagName("br");
+    const element = document.getElementsByTagName("h1");
  
     document.getElementById("demo").innerHTML = 'The text in first paragraph (index 0) is: ' + element[0].innerHTML;
  
