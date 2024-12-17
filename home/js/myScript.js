7d2c00bc6d4dbb9b4632968cd8436609c03427ee
Airo0429 <91652791+Airo0429@users.noreply.github.com>
2024-12-17 19:41:00 +0800

    lala

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -7,6 +7,6 @@ function myFunction2() {
     const element = document.getElementsByTagName("p");
  
    
-    document.getElementById("demo").innerHTML = '<br> The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
+    document.getElementById("demo").innerHTML = '<p> The text in first paragraph (index 0) is: <p>' + element[0].innerHTML;
  
 }
